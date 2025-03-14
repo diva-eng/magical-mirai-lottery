5702585dbfbfccfe2ada17b66a968a51505d225c
Siyuan Gao <siyuangao@pm.me>
2025-03-14 13:54:47 -0700

    fix encoding

--- run.bat
+++ run.bat
@@ -7,75 +7,75 @@ cd /d %~dp0
 :: Check if Node.js is installed
 node -v >nul 2>&1
 if %errorlevel% neq 0 (
-  echo 未安装 Node.js。正在安装 Node.js...
+  echo δ��װ Node.js�����ڰ�װ Node.js...
   :: Download and install Node.js
   powershell -Command "Start-Process msiexec.exe -ArgumentList '/i https://nodejs.org/dist/v22.14.0/node-v22.14.0-x64.msi /quiet /norestart' -NoNewWindow -Wait"
   if %errorlevel% neq 0 (
-    echo 安装 Node.js 失败。正在退出...
+    echo ��װ Node.js ʧ�ܡ������˳�...
     pause
     exit /b 1
   )
 )
 
 :: install dependencies
-echo 正在安装依赖项...
+echo ���ڰ�װ������...
 call npm install >nul 2>&1
 if %errorlevel% neq 0 (
-  echo 安装依赖项失败。正在退出...
+  echo ��װ������ʧ�ܡ������˳�...
   pause
   exit /b 1
 )
 
 :: install playwright dependency
-echo 正在安装 Playwright 依赖项...
+echo ���ڰ�װ Playwright ������...
 call npm install playwright >nul 2>&1
 if %errorlevel% neq 0 (
-  echo 安装 Playwright 依赖项失败。正在退出...
+  echo ��װ Playwright ������ʧ�ܡ������˳�...
   pause
   exit /b 1
 )
 
 :: install playwright browsers
-echo 正在安装 Playwright 浏览器...
+echo ���ڰ�װ Playwright �����...
 set PLAYWRIGHT_BROWSERS_PATH=0
 call npx playwright install >nul 2>&1
 if %errorlevel% neq 0 (
-  echo 安装 Playwright 浏览器失败。正在退出...
+  echo ��װ Playwright �����ʧ�ܡ������˳�...
   pause
   exit /b 1
 )
 
 echo ================================
-echo 魔法未来国内申请填表器
+echo ħ��δ���������������
 echo ================================
 
 :menu
-echo 请选择一个选项:
-echo 1. 以测试模式运行脚本
-echo 2. 以提交模式运行脚本
-echo 3. 退出
-set /p choice="输入你的选择: "
+echo ��ѡ��һ��ѡ��:
+echo 1. �Բ���ģʽ���нű�
+echo 2. ���ύģʽ���нű�
+echo 3. �˳�
+set /p choice="�������ѡ��: "
 
 if "%choice%"=="1" (
   set mode=dryrun
 ) else if "%choice%"=="2" (
   set mode=real
 ) else if "%choice%"=="3" (
-  echo 退出中...
+  echo �˳���...
   exit /b 0
 ) else (
-  echo 无效的选择。请重试。
+  echo ��Ч��ѡ�������ԡ�
   goto menu
 )
 
 :: Execute index.js using Node.js
-echo 请确保已在 applications.csv 中修改并添加所有申请条目。
-set /p confirm="确认继续? (y/n): "
+echo ��ȷ������ applications.csv ���޸Ĳ���������������Ŀ��
+set /p confirm="ȷ�ϼ���? (y/n): "
 if /i not "%confirm%"=="y" (
-  echo 操作已取消。
+  echo ������ȡ����
   exit /b 0
 )
-echo 正在运行 index.js...
+echo �������� index.js...
 if "%mode%"=="dryrun" (
   node index.js --dry-run
 ) else (
